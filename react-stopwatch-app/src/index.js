641774f4863df23031efbb5800099e8bc552c6d3
LiamNixon <liam_nixon@outlook.com>
2022-09-07 16:07:06 +1000

    Create stopwatch app
    A simple stopwatch app created in ReactJS! Includes a user interface which provides start/stop/reset functionality.

---
+++ react-stopwatch-app/src/index.js
@@ -0,0 +1,11 @@
+// Import modules
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// Components
+import Stopwatch from './components/Stopwatch';
+
+// Stylesheet
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Stopwatch />);
