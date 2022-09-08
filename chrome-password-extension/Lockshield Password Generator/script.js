023d187b014f8fee72bd43533fd839b5ae99f713
LiamNixon <liam_nixon@outlook.com>
2022-09-08 16:44:23 +1000

    Add user interactions
    Added user interactivity to the password generator. It now accepts various parameters and has input validation to ensure no unwanted results occur. It can also store any generated password to the clipboard now for convenience! Next up, a little cosmetic refinement!

--- chrome-password-extension/Lockshield Password Generator/script.js
+++ chrome-password-extension/Lockshield Password Generator/script.js
@@ -11,6 +11,21 @@ let settingsToggle = false;
 let upperLowerToggle = false;
 let numbersToggle = false;
 let specialCharactersToggle = false;
+let lettersToggle = true;
+let pinToggle = false;
+let desiredLength = 4;
+
+const generateButton = document.querySelector('#generateButton');
+const saveButton = document.querySelector('#saveButton');
+const settingsButton = document.querySelector('#settingsButton');
+const lengthField = document.querySelector('#lengthField');
+const mixToggle = document.querySelector('#mixToggle');
+const numberToggle = document.querySelector('#numberToggle');
+const specialToggle = document.querySelector('#specialToggle');
+const letterToggle = document.querySelector('#letterToggle');
+const pinToggleEl = document.querySelector('#pinToggle');
+
+lengthField.value = desiredLength;
 
 // Methods
 const randomNumber = (min, max) => {
@@ -21,35 +36,38 @@ const randomNumber = (min, max) => {
 
 const generatePassword = () => {
     let passwordOutput = '';
-    const max = 6;
-    console.log(randomNumber(1,4));
-    
-    for (let i = 0; i < max; i++) {
-        switch(randomNumber(1,4)) {
-            case 1:
-                console.log('1');
-                passwordOutput = passwordOutput + lowercaseCharacters.at(randomNumber(0,lowercaseCharacters.length - 1));
-                break;
-            case 2:
-                console.log('2');
-                passwordOutput = passwordOutput + uppercaseCharacters.at(randomNumber(0,uppercaseCharacters.length - 1));
-                break;
-            case 3:
-                console.log('3');
-                passwordOutput = passwordOutput + numbers.at(randomNumber(0,numbers.length - 1));
-                break;
-            case 4:
-                console.log('4');
-                passwordOutput = passwordOutput + specialCharacters.at(randomNumber(0,specialCharacters.length - 1));
-                break;
-        };
+    let optionsList = [];
+    let selectedList = [];
+
+    if (lettersToggle) {
+        optionsList.push(lowercaseCharacters);
+    }
+
+    if (upperLowerToggle) {
+        optionsList.push(uppercaseCharacters);
     };
-    console.log(passwordOutput);
+
+    if (numbersToggle) {
+        optionsList.push(numbers);
+    };
+
+    if (specialCharactersToggle) {
+        optionsList.push(specialCharacters);
+    };
+
+    if (optionsList.length != 0) {
+        for (let i = 0; i < desiredLength; i++) {
+            selectedList = optionsList[randomNumber(0, optionsList.length - 1)];
+            passwordOutput = passwordOutput + selectedList[randomNumber(0, selectedList.length - 1)];
+        };
+    } else {
+        passwordOutput = '* * *';
+    }
     passwordField.innerHTML = passwordOutput;
 };
 
 const savePassword = () => {
-
+    navigator.clipboard.writeText(passwordField.innerHTML);
 }
 
 const toggleSettings = () => {
@@ -64,7 +82,82 @@ const toggleSettings = () => {
     };
 };
 
+const toggleMix = () => {
+    if (pinToggleEl.checked === true || letterToggle.checked === true) {
+        mixToggle.checked = false;
+    } else {
+        upperLowerToggle = !upperLowerToggle;
+    };
+};
+
+const toggleNumbers = () => {
+    if (pinToggleEl.checked === true) {
+        numberToggle.checked = true;
+    } else {
+        numbersToggle = !numbersToggle;
+    };
+};
+
+const toggleSpecial = () => {
+    if (pinToggleEl.checked === true) {
+        specialToggle.checked = false;
+    } else {
+        specialCharactersToggle = !specialCharactersToggle;
+    };
+};
+
+const toggleLetters = () => {
+    if (letterToggle.checked === true) {
+        lettersToggle = false;
+        mixToggle.checked = false;
+        pinToggle = false;
+        pinToggleEl.checked = false;
+        upperLowerToggle = false;
+    } else {
+        lettersToggle = true;
+    }
+};
+
+const togglePIN = () => {
+    pinToggle = !pinToggle;
+    if (pinToggleEl.checked === true) {
+        lengthField.value = 4;
+        desiredLength = 4;
+        upperLowerToggle = false;
+        mixToggle.checked = false;
+        numbersToggle = true;
+        numberToggle.checked = true;
+        specialCharactersToggle = false;
+        specialToggle.checked = false;
+        lettersToggle = false;
+        letterToggle.checked = false;
+    };
+};
+
+const setLength = (e) => {
+    if (pinToggle === true) {
+        desiredLength = 4;
+        e.target.value = 4;
+    } else {
+        if (e.target.value >= 4 && e.target.value <= 12) {
+            desiredLength = e.target.value;
+        } else if (e.target.value < 4) {
+            desiredLength = 4;
+            e.target.value = 4;
+        } else {
+            desiredLength = 12;
+            e.target.value = 12;
+        };
+    };
+};
+
 // Event Listeners
-document.querySelector('#generateButton').addEventListener('click', generatePassword);
-document.querySelector('#saveButton').addEventListener('click', savePassword);
-document.querySelector('#settingsButton').addEventListener('click', toggleSettings);
+generateButton.addEventListener('click', generatePassword);
+saveButton.addEventListener('click', savePassword);
+settingsButton.addEventListener('click', toggleSettings);
+lengthField.addEventListener('input', setLength);
+mixToggle.addEventListener('change', toggleMix);
+numberToggle.addEventListener('change', toggleNumbers);
+specialToggle.addEventListener('change', toggleSpecial);
+letterToggle.addEventListener('change', toggleLetters);
+pinToggleEl.addEventListener('change', togglePIN);
